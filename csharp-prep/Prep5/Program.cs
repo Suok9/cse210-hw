a1e2f7241a035ec349e5ec928e2565415a83d114
Suok9 <suoephraim@gmail.com>
2024-09-27 03:32:24 +0100

    Update Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,44 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string userName = GetUserName();
+        int userNumber = GetUserNumber();
+        int squaredNumber = CalculateSquare(userNumber);
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Square Calculator!");
+    }
+
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine().Trim();
+    }
+
+    static int GetUserNumber()
+    {
+        while (true)
+        {
+            Console.Write("Please enter your favorite number: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+                return number;
+            Console.WriteLine("Invalid input. Please enter a whole number.");
+        }
+    }
+
+    static int CalculateSquare(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
     }
 }
