e3caca4f999dadd082561d729ce391093ecc60a2
Suok9 <suoephraim@gmail.com>
2024-10-24 04:15:19 +0100

    Update Program.cs

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 5),
+            new Rectangle("Blue", 4, 6),
+            new Circle("Green", 3)
+        };
+
+        // Iterate through the list and display color and area
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.Color} shape has an Area of {shape.GetArea():F2}");
+        }
     }
 }
